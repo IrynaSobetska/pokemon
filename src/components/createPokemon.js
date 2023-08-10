c61a739109961d971de9d0731c95051d70909cae
IrynaSobetska <sobetska.ira@gmail.com>
2023-08-10 15:29:30 +0200

    update pokemon

--- src/components/createPokemon.js
+++ src/components/createPokemon.js
@@ -1,14 +1,17 @@
 const createPokemon = (pokemonData) => {
     const container = document.createElement('div');
-    container.classList = 'container';
+    container.id = 'container';
 
     const title = document.createElement('h2');
     title.innerText = pokemonData.name;
+    title.id = 'title';
 
     const img = document.createElement('img');
     img.src = pokemonData.sprites.front_default;
+    img.id = 'img';
 
     const abilities = document.createElement('div');
+    abilities.id = 'abilities';
     const abilTitle = document.createElement('h3');
     abilTitle.innerHTML = 'Abilities:';
     abilities.append(abilTitle);
